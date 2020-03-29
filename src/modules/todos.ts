da95cb50437832f47f699335ed3a7cf556faf54f
Suemin Jeong <sueminee@gmail.com>
2020-03-29 21:31:06 +0900

    Refactoring todo.ts with typesafe-actions library.

--- src/modules/todos.ts
+++ src/modules/todos.ts
@@ -1,33 +1,41 @@
+import { createAction, ActionType, createReducer } from 'typesafe-actions';
+// createStandardAction 없어짐 대신 createAction 사용.
+
 //1. Action type
-const ADD_TODO = 'todos/ADD_TODO' as const;
-const TOGGLE_TODO = 'todos/TOGGLE_TODO' as const;
-const REMOVE_TODO = 'todos/REMOVE_TODO' as const;
+const ADD_TODO = 'todos/ADD_TODO';
+const TOGGLE_TODO = 'todos/TOGGLE_TODO';
+const REMOVE_TODO = 'todos/REMOVE_TODO';
 
 
 
 //액션생성함수
-export const addTodo = (text: string) => ({
-  type: ADD_TODO,
-  payload: text
-});
+// export const addTodo = (text: string) => ({
+//   type: ADD_TODO,
+//   payload: text
+// });
 
-export const toggleTodo = (id: number) =>({
-  type: TOGGLE_TODO,
-  payload: id
-});
+// export const toggleTodo = (id: number) =>({
+//   type: TOGGLE_TODO,
+//   payload: id
+// });
 
-export const removeTodo = (id: number) => ({
-  type: REMOVE_TODO,
-  payload: id
-})
-
+// export const removeTodo = (id: number) => ({
+//   type: REMOVE_TODO,
+//   payload: id
+// })
+//typesafe-actions로 리팩토링
+export const addTodo = createAction(ADD_TODO)<string>();
+export const toggleTodo = createAction(TOGGLE_TODO)<number>();
+export const removeTodo = createAction(REMOVE_TODO)<number>();
 
 //액션의 ts타입 선언
-type TodosAction =
-  | ReturnType<typeof addTodo>
-  | ReturnType<typeof toggleTodo>
-  | ReturnType<typeof removeTodo>;
-
+// type TodosAction =
+//   | ReturnType<typeof addTodo>
+//   | ReturnType<typeof toggleTodo>
+//   | ReturnType<typeof removeTodo>;
+//typesafe-actions로 리팩토링
+const actions = { addTodo, toggleTodo, removeTodo };
+type TodosAction = ActionType<typeof actions>;
 
 
 // 상태를 위한 타입 선언
@@ -47,24 +55,36 @@ const initialState: TodosState = [
 ];
 
 //리듀서 구현하기
-function todos(state: TodosState = initialState, action: TodosAction): TodosState {
-  switch (action.type) {
-    case ADD_TODO:
-      const nextId = Math.max(...state.map(todo => todo.id)) + 1;
-      // 현재 상태의 모든 항목들의 id를 체크해서, 그중 가장 큰값 +1 한 값이 새 id가 된다.
-      return state.concat({
-        id: nextId,
-        text: action.payload,
-        done: false
-      });
-    case TOGGLE_TODO:
-      return state.map(todo => todo.id === action.payload ? {...todo, done: !todo.done} : todo);
-      // 클릭된 id의 todo는 done의 상태가 토글되고, 클릭된 id가 아닌애들(나머지)은 원래 그대로.
-    case REMOVE_TODO:
-      return state.filter(todo => todo.id !== action.payload);
-      // remove하는 id 가 아닌 애들만, TodosState가 된다.
-    default:
-      return state;
-  }
-}
+// function todos(state: TodosState = initialState, action: TodosAction): TodosState {
+//   switch (action.type) {
+//     case ADD_TODO:
+//       const nextId = Math.max(...state.map(todo => todo.id)) + 1;
+//       // 현재 상태의 모든 항목들의 id를 체크해서, 그중 가장 큰값 +1 한 값이 새 id가 된다.
+//       return state.concat({
+//         id: nextId,
+//         text: action.payload,
+//         done: false
+//       });
+//     case TOGGLE_TODO:
+//       return state.map(todo => todo.id === action.payload ? {...todo, done: !todo.done} : todo);
+//       // 클릭된 id의 todo는 done의 상태가 토글되고, 클릭된 id가 아닌애들(나머지)은 원래 그대로.
+//     case REMOVE_TODO:
+//       return state.filter(todo => todo.id !== action.payload);
+//       // remove하는 id 가 아닌 애들만, TodosState가 된다.
+//     default:
+//       return state;
+//   }
+// }
+//typesafe-actions로 리팩토링
+const todos = createReducer<TodosState, TodosAction>(initialState, {
+  [ADD_TODO]: (state, {payload: text }) =>
+    state.concat({
+      id: Math.max(...state.map(todo => todo.id)) +1,
+      text,
+      done: false
+    }),
+    [TOGGLE_TODO]: (state, {payload: id}) => state.map(todo => (todo.id === id ? {...todo, done:!todo.done} : todo)),
+    [REMOVE_TODO]: (state, {payload: id}) => state.filter(todo => todo.id !== id)
+})
+
 export default todos;
